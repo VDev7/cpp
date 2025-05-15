54c469daccc551e90dc0c3795005b1295cf089e8
VDev7 <152139316+VDev7@users.noreply.github.com>
2025-05-15 14:29:13 +0700

    Update algorithm.hpp

--- algorithm.hpp
+++ algorithm.hpp
@@ -3,34 +3,169 @@
 
 #include <iostream>
 #include <vector>
-#include <string>
-#include <random>
 
 namespace cpp
 {
     template <typename T>
-    bool is_sorted(const T a);
+    bool is_sorted(const T a)
+    {
+        bool is_ascending = 1;
+        bool is_descending = 1;
+    
+        for (size_t i = 0; i < a.size() - 1; i++)
+        {
+            if (a[i] > a[i + 1]) is_ascending = 0;
+            if (a[i] < a[i + 1]) is_descending = 0;
+        }
+    
+        if (is_debugging)
+        {
+            if (is_ascending) std::cout << "The container is sorted and ascending" << '\n';
+            else if (is_descending) std::cout << "The container is sorted and descending" << '\n';
+            else std::cout << "The a isn't sorted" << '\n';
+        }
+    
+        return is_ascending || is_descending;
+    }
 
     template <typename T>
-    void shuffle(std::vector<T>& a);
+    void print_vector(const std::vector<T> a)
+    {
+        std::cout << "[";
+    
+        for (size_t i = 0; i < a.size(); i++)
+        {
+            std::cout << a[i];
+    
+            if (i != a.size() - 1) std::cout << ", ";
+        }
+    
+        std::cout << "]" << '\n';
+    }
 
     template <typename T>
-    void print_vector(const std::vector<T> a);
+    int linear_search(std::vector<T> a)
+    {
+        int n = a.size();
+        T input;
+    
+        std::cout << "linear_search::input: ";
+        std::cin >> input;
+    
+        for (int i = 0; i < n; i++)
+        {
+            if (a[i] == input)
+            {
+                std::cout << input << " Found at index number " << i << '\n';
+                return i;
+            }
+        }
+    
+        std::cout << input << " Can't be found" << '\n';
+        return 0;
+    }
+
+    template <typename T>
+    int binary_search(std::vector<T> a)
+    {
+        int l = 0;
+        int r = a.size() - 1;
+        T input;
+    
+        if (!is_sorted(a))
+        {
+            std::cout << "binary_search::Container isn't sorted, unable to search" << '\n';
+    
+            return 0;
+        }
+    
+        std::cout << "binary_search::Input data: ";
+        std::cin >> input;
+    
+        while (l <= r)
+        {
+            int m = l + (r - l) / 2;
+    
+            if (a[m] == input)
+            {
+                std::cout << input << " Found at index number " << m << '\n';
+                return m;
+            }
+    
+            else if (a[m] < input) l = m + 1;
+            else r = m - 1;
+        }
+    
+        std::cout << input << " Can't be found" << '\n';
+        return 0;
+    }
 
     template<typename T>
-    int linear_search(std::vector<T> a);
-
-    template<typename T>
-    int binary_search(std::vector<T> a);
-
-    template<typename T>
-    void bubble_sort(std::vector<T>& a);
+    void bubble_sort(std::vector<T>& a)
+    {
+        bool is_swapped;
+    
+        for (size_t i = 0; i < a.size() - 1; i++)
+        {
+            is_swapped = 0;
+    
+            for (size_t j = 0; j < a.size() - 1; j++)
+            {
+                if (a[j] > a[j + 1])
+                {
+                    T temp = a[j];
+                    a[j] = a[j + 1];
+                    a[j + 1] = temp;
+    
+                    is_swapped = 1;
+    
+                    if (is_debugging) print_vector(a);
+                }
+            }
+    
+            if (!is_swapped) break;
+        }
+    }
 
     template <typename T>
-    void selection_sort(std::vector<T>& a);
+    void selection_sort(std::vector<T>& a)
+    {
+        int n = a.size();
+    
+        for (int i = 0; i < n - 1; i++)
+        {
+            int min = i;
+    
+            for (int j = i + 1; j < n; j++) if (a[j] < a[min]) min = j;
+    
+            T temp = a[i];
+            a[i] = a[min];
+            a[min] = temp;
+    
+            if (is_debugging) print_vector(a);
+        }
+    }
 
     template <typename T>
-    void insertion_sort(std::vector<T>& a);
+    void insertion_sort(std::vector<T>& a)
+    {
+        int n = a.size();
+    
+        for(int i = 1; i < n; i++)
+        {
+            T key = a[i];
+            int j = i - 1;
+    
+            while (j >= 0 && a[j] > key)
+            {
+                a[j + 1] = a[j];
+                --j;
+            }
+    
+            a[j + 1] = key;
+            if (is_debugging) print_vector(a);
+        }
+    }
 }
 
 #endif
